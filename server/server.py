979dadf6a77074f9af1e3debfc5b5e91c9e5b6a4
Tin Duong <tin.duong405@gmail.com>
2025-03-16 16:08:34 -0500

    started on socket setup and now need to finish it

--- server/server.py
+++ server/server.py
@@ -1,8 +1,18 @@
-import socket # Import the built-in socket module
+import socket # Import the python socket module
 
-socket_init = socket.socket()
-print("Socket connected!")
 
-port = 19791
+class Server: # OOP opproach for application
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((self.host, self.port))
+        self.server.listen(5)
+        print(f"Server is listening on {self.host}:{self.port}")
 
-socket_init.binf(('', socket_init))
+    def run(self):
+        while True:
+            client, address = self.server.accept()
+            print(f"Connection from {address} is successful!")
+            client.send("Welcome to the chatroom to start by typing in the command line".encode())
+            client.close()
